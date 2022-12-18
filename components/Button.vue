c89e25b636534eeb7c01169579def4ab89926ad3
redbayoub <redayoub47@gmail.com>
2022-12-18 14:36:35 +0100

    update button

--- components/Button.vue
+++ components/Button.vue
@@ -1,21 +1,63 @@
 <template>
   <button
     @click="onClick && onClick()"
-    class="bg-purple-500 text-white py-2 px-6 rounded-md hover:bg-purple-600"
+    :class="classObject"
+    class="text-white py-2 px-6 rounded-md disabled:bg-gray-400"
+    :disabled="loading"
   >
-    <slot />
+    <div class="flex gap-3 justify-center items-center">
+      <Spinner v-if="loading" />
+      <slot />
+    </div>
   </button>
 </template>
 
 <script>
 export default {
   props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
     onClick: {
       type: Function,
+    },
+    type: {
+      type: String,
+      default: 'primary',
+    },
+  },
+  computed: {
+    classObject() {
+      switch (this.type) {
+        case 'primary':
+          return {
+            'bg-purple-600': true,
+            'hover:bg-purple-500': true,
+          }
+        case 'success':
+          return {
+            'bg-green-600': true,
+            'hover:bg-green-500': true,
+          }
+        case 'warning':
+          return {
+            'bg-yellow-600': true,
+            'hover:bg-yellow-500': true,
+          }
+        case 'danger':
+          return {
+            'bg-red-600': true,
+            'hover:bg-red-500': true,
+          }
+        case 'light':
+          return {
+            'bg-gray-300': true,
+            'hover:bg-gray-200': true,
+            'text-gray-600': true,
+          }
+      }
     },
   },
 }
 </script>
-
-<style>
-</style>
---
+++ components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="animate-spin inline-block w-5 h-5 border-[3px] border-current border-t-transparent rounded-full"
+    role="status"
+    aria-label="loading"
+  >
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
