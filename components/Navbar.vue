a6e3b2cf9f5bd20162cb86adccf64a8e3ab0bc8c
redbayoub <redayoub47@gmail.com>
2022-12-18 14:39:39 +0100

    update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,22 +1,16 @@
 <template>
   <nav id="header" class="w-full z-30">
     <div class="w-full flex items-center justify-between py-2">
-      <span class="font-medium text-lg">Auction</span>
+      <NuxtLink to="/">
+        <span class="font-medium text-lg">Auction</span>
+      </NuxtLink>
 
       <div
-        class="
-          order-2
-          md:order-3
-          flex flex-wrap
-          items-center
-          justify-end
-          mr-0
-          md:mr-4
-        "
+        class="order-2 md:order-3 flex flex-wrap items-center justify-end"
         id="nav-content"
       >
-        <div class=" flex items-center w-full md:w-full">
-          <Button :onClick="logout">Logout</Button>
+        <div v-if="isLoggedIn" class="flex items-center w-full md:w-full">
+          <Button :loading="inProgress" :onClick="logout">Logout</Button>
         </div>
       </div>
     </div>
@@ -25,11 +19,29 @@
 
 <script>
 export default {
+  computed: {
+    isLoggedIn() {
+      return this.$auth.loggedIn
+    },
+  },
+  data() {
+    return {
+      inProgress: false,
+    }
+  },
+
   methods: {
     async logout() {
-      await this.$auth.logout()
+      try {
+        this.inProgress = true
+        await this.$auth.logout()
 
-      this.$router.push('/login')
+        this.$router.push('/login')
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.inProgress = false
+      }
     },
   },
 }
