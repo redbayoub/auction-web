59ffcafc09190fdedc7494608e598489df22f538
redbayoub <redayoub47@gmail.com>
2022-12-18 14:38:16 +0100

    update index page

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container px-2 mx-auto flex flex-col h-screen">
+    <Navbar />
+    <div class="py-6 sm:py-12">
+      <Nuxt />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,9 @@
-<template>
-  <div class="container px-2 mx-auto flex flex-col h-screen">
-    <Navbar />
-
-    <h1 class="text-3xl font-bold underline">
-      Hello world! {{ user.username }}
-    </h1>
-  </div>
-</template>
-
 <script>
 export default {
   name: 'IndexPage',
-  data() {
-    return {
-      user: this.$auth.user.data,
-    }
+  middleware({ $auth, redirect }) {
+    if ($auth.user.data.isAdmin) return redirect('/dashboard')
+    else return redirect('/items')
   },
 }
 </script>
