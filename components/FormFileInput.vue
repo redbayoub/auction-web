bfbb686c4fb2af48d3384fced75c3b2125577f85
redbayoub <redayoub47@gmail.com>
2022-12-18 14:43:01 +0100

    add dashboard add item page

---
+++ components/FormFileInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label class="block font-semibold"> {{ label }} </label>
+
+    <input
+      class="mt-2 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 file:mr-4 file:py-2 file:px-4 file:rounded-l-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100  disabled:bg-gray-100 disabled:text-gray-400"
+      @change="$emit('change', $event)"
+      v-bind="$attrs"
+      type="file"
+    />
+
+    <p class="text-xs text-red-400" v-if="error">
+      {{ error }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value', 'label', 'error'],
+}
+</script>
+
+<style></style>
---
+++ components/FormTextArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label class="block font-semibold"> {{ label }} </label>
+    <textarea
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      v-bind="$attrs"
+      class="px-3  mt-2 hover:outline-none focus:outline-none focus:ring-1 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  disabled:bg-gray-100 disabled:text-gray-400"
+      rows="3"
+    ></textarea>
+    <p class="text-xs text-red-400" v-if="error">
+      {{ error }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value', 'label', 'error'],
+}
+</script>
+
+<style></style>
