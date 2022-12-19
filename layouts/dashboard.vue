4664d99adace42fa044b9271f1566c2b221e8df5
redbayoub <redayoub47@gmail.com>
2022-12-19 09:21:08 +0100

    update dashboard layout

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container px-2 mx-auto flex flex-col h-screen">
     <Navbar class="px-2" />
-    <div class="mt-5">
+    <div class="py-5">
       <Nuxt />
     </div>
   </div>
