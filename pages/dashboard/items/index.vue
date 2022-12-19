1a6ffda21a2d93f88e81da2fac92fdf80cacb774
redbayoub <redayoub47@gmail.com>
2022-12-19 09:21:36 +0100

    fix issue in dashboard list items page

--- pages/dashboard/items/index.vue
+++ pages/dashboard/items/index.vue
@@ -99,7 +99,6 @@
             }"
             v-for="(item, index) in items"
             :key="item.id"
-            :item="item"
           >
             <td
               class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
