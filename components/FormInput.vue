cd83fee357805f4104fef694c078dac87f52fd2c
redbayoub <redayoub47@gmail.com>
2022-12-19 09:03:14 +0100

    update FormInput

--- components/FormInput.vue
+++ components/FormInput.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <label v-if="label"  class="block font-semibold"> {{ label }} </label>
+    <label v-if="label"  class="block font-semibold mb-2"> {{ label }} </label>
     <input
       :value="value"
       @input="$emit('input', $event.target.value)"
       v-bind="$attrs"
-      class="h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:text-gray-400 "
+      class="h-5 px-3 py-5  hover:outline-none focus:outline-none focus:ring-1 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:text-gray-400 "
     />
     <p class="text-xs text-red-400" v-if="error">
       {{ error }}
