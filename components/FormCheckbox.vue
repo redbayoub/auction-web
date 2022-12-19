044c887ed9dc3fa197f091ae8c794d6da32ac505
redbayoub <redayoub47@gmail.com>
2022-12-19 16:45:36 +0100

    add auto-bid feature

---
+++ components/FormCheckbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="flex items-center gap-2">
+      <input
+        :checked="value"
+        @input="$emit('input', $event.target.checked)"
+        type="checkbox"
+        v-bind="$attrs"
+        class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600"
+      />
+      <label v-if="label" class="text-sm font-medium text-gray-900">
+        {{ label }}
+      </label>
+    </div>
+    <p class="text-xs text-red-400" v-if="error">
+      {{ error }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value', 'label', 'error'],
+}
+</script>
+
+<style></style>
--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -23,6 +23,40 @@
           type="info"
         />
         <div class="mb-2" v-else-if="!isAuctionClosed">
+          <form
+            method="post"
+            @submit.prevent="submitToggleAutoBid()"
+            class="mb-3 py-3 px-3 bg-gray-50 rounded max-w-sm"
+          >
+            <div class="flex gap-3 items-center justify-between">
+              <FormCheckbox
+                label="Allow bot to auto-bid on this item"
+                v-model="toggleAutoBid"
+                :disabled="inAutoBidProgress || !isAutoBidBotConfigured"
+                :error="autoBidError?.message"
+              />
+              <Button
+                :class="{
+                  'opacity-0 pointer-events-none':
+                    toggleAutoBid == isAutoBidEnabled,
+                }"
+                :loading="inAutoBidProgress"
+                type="light"
+                >Save</Button
+              >
+            </div>
+
+            <p class="text-gray-400 mt-3" v-if="!isAutoBidBotConfigured">
+              To use this feature you need to configure the bot setting from
+              <NuxtLink
+                class="text-blue-500 hover:text-blue-600"
+                to="/user-settings"
+              >
+                User Setting Page</NuxtLink
+              >
+            </p>
+          </form>
+
           <form method="post" @submit.prevent="submitBid()" class="flex gap-3">
             <FormInput
               v-model="amount"
@@ -53,7 +87,17 @@ export default {
     const item = response.data
     const amount = item.price + 1
     const currentUserId = $auth.user.data.id
-    return { item, id, amount, currentUserId }
+    let isAutoBidEnabled = false
+    try {
+      const autoBidResponse = await $axios.$get(
+        '/api/items/' + id + '/auto-bid'
+      )
+      isAutoBidEnabled = autoBidResponse.data?.id !== undefined
+    } catch (error) {}
+
+    const toggleAutoBid = isAutoBidEnabled
+
+    return { item, id, amount, currentUserId, isAutoBidEnabled, toggleAutoBid }
   },
   computed: {
     bids_closes_at() {
@@ -63,12 +107,19 @@ export default {
     isAuctionClosed() {
       return this.bids_closes_at < new Date()
     },
+
+    isAutoBidBotConfigured() {
+      return this.$auth.user.data.bot != null
+    },
   },
   data() {
     return {
       amount: null,
       inProgress: false,
       error: null,
+      toggleAutoBid: false,
+      inAutoBidProgress: false,
+      autoBidError: null,
     }
   },
   methods: {
@@ -87,6 +138,23 @@ export default {
         this.error = e.response.data
       } finally {
         this.inProgress = false
+      }
+    },
+
+    async submitToggleAutoBid() {
+      try {
+        this.inAutoBidProgress = true
+        this.toggleAutoBid
+          ? await this.$axios.$post('/api/items/' + this.item.id + '/auto-bid')
+          : await this.$axios.$delete(
+              '/api/items/' + this.item.id + '/auto-bid'
+            )
+
+        this.$nuxt.refresh()
+      } catch (e) {
+        this.autoBidError = e.response.data
+      } finally {
+        this.inAutoBidProgress = false
       }
     },
   },
