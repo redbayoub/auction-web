b264aa643b1ef9f6973844343cd78f46ea0a11ce
redbayoub <redayoub47@gmail.com>
2022-12-20 10:36:34 +0100

    refactor navbar dropdown

---
+++ components/NavDropdown.vue
@@ -0,0 +1,49 @@
+<template>
+  <li class="relative">
+    <button
+      @click.prevent="showDropdown = !showDropdown"
+      id="dropdownNavbarLink"
+      data-dropdown-toggle="dropdownNavbar"
+      class="text-gray-700 pl-3 pr-4 py-2 hover:text-blue-700 md:p-0 font-medium flex items-center justify-between w-full md:w-auto"
+    >
+      <slot name="button"></slot>
+      <svg
+        :class="{
+          'rotate-180': showDropdown,
+        }"
+        class="w-4 h-4 ml-1"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+    <!-- Dropdown menu -->
+    <div
+      id="dropdownNavbar"
+      :class="{
+        hidden: !showDropdown,
+      }"
+      class="text-base z-10 list-none divide-y divide-gray-100 rounded shadow-xl bg-gray-50 w-44 absolute right-0"
+    >
+      <ul class="py-1" aria-labelledby="dropdownLargeButton">
+        <slot v-bind:showDropdown="showDropdown" name="items"></slot>
+      </ul>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDropdown: false,
+    }
+  },
+}
+</script>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,65 +8,39 @@
         <ul
           class="flex-col md:flex-row flex md:space-x-8 md:text-sm md:font-medium"
         >
-          <li v-if="isLoggedIn" class="relative">
-            <button
-              @click.prevent="showDropdown = !showDropdown"
-              id="dropdownNavbarLink"
-              data-dropdown-toggle="dropdownNavbar"
-              class="text-gray-700 pl-3 pr-4 py-2 hover:text-blue-700 md:p-0 font-medium flex items-center justify-between w-full md:w-auto"
-            >
+          <NavDropdown v-if="isLoggedIn">
+            <template v-slot:button>
               {{ username }}
-              <svg
-                :class="{
-                  'rotate-180': showDropdown,
-                }"
-                class="w-4 h-4 ml-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <!-- Dropdown menu -->
-            <div
-              id="dropdownNavbar"
-              :class="{
-                hidden: !showDropdown,
-              }"
-              class="text-base z-10 list-none divide-y divide-gray-100 rounded shadow-xl bg-gray-50 w-44 absolute right-0"
-            >
-              <ul class="py-1" aria-labelledby="dropdownLargeButton">
-                <li v-if="!isAdmin">
-                  <span
-                    v-if="$route.path == '/user-settings'"
-                    class="text-sm block px-4 py-2 text-blue-700"
-                  >
-                    Settings
-                  </span>
-                  <NuxtLink
-                    v-else
-                    to="/user-settings"
-                    @click.native="showDropdown = !showDropdown"
-                    class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-                    >Settings</NuxtLink
-                  >
-                </li>
-              </ul>
-              <div class="py-1">
+            </template>
+
+            <template v-slot:items="slotProps">
+              <li v-if="!isAdmin">
+                <span
+                  v-if="$route.path == '/user-settings'"
+                  class="text-sm block px-4 py-2 text-blue-700"
+                >
+                  Settings
+                </span>
+                <NuxtLink
+                  v-else
+                  to="/user-settings"
+                  @click.native="
+                    slotProps.showDropdown = !slotProps.showDropdown
+                  "
+                  class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
+                  >Settings</NuxtLink
+                >
+              </li>
+              <li class="py-1">
                 <a
                   href="#"
                   @click.prevent="logout()"
                   class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
                   >Sign out</a
                 >
-              </div>
-            </div>
-          </li>
+              </li>
+            </template>
+          </NavDropdown>
         </ul>
       </div>
     </div>
@@ -90,7 +64,6 @@ export default {
   data() {
     return {
       inProgress: false,
-      showDropdown: false,
     }
   },
 
